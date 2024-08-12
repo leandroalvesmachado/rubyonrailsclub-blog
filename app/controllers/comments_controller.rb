96499e29fc726ee0762edd0c2fca46229540849c
leandroalvesmachado <leandroalvesmachado@gmail.com>
2024-08-12 15:11:36 -0300

    Fim Aula 82 - Cadastrando novos comentários a partir do artigo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :set_article, only: [:index]
+  before_action :set_article, only: [:create]
 
-  def index
-    @comments = @article.comments
+  def create
+    @article.comments.create!(comment_params)
+
+    redirect_to(article_path(@article))
   end
 
   private
 
   def set_article
     @article = Article.friendly.find(params[:article_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :article
 
   # Sempre inclui user e article nas consultas
